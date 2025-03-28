e155cf43d18542ceacf682a666a6f2d4cdd7be19
eunbie07 <eunbie07@gmail.com>
2025-03-28 14:28:51 +0900

    mysql_appjs

---
+++ node/mysql/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var mysql = require('mysql');
+const env = require('dotenv').config({ path: "../../.env" });
+var app = express();
+
+var connection = mysql.createConnection({
+    host: process.env.NODE_MYSQL_HOST,
+    user: process.env.NODE_MYSQL_USER,
+    password: process.env.NODE_MYSQL_PASSWORD,
+    database: process.env.NODE_MYSQL_DATABASE
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('Database is conneted~!!\n\n');
+    } else {
+        console.log('Error connectiong Database~!!\n\n');
+    }
+});
+
+app.get('/', function (req, res) {
+    connection.query('SELECT * FROM st_info', function (err, rows, fields) {
+        connection.end();
+        if (err) {
+            res.send(rows);
+            console.log("The solution is : ", rows);
+        } else {
+            console.log('Error while performing Query~!!\n\n');
+        }
+    })
+})
+
+app.listen(8000, function() {
+    console.log("8000 Port : Server Started~!!\n\n");
+})
