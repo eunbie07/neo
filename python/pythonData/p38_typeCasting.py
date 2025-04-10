946d3df633f9027bc81e21332654d8734bd45b12
eunbie07 <eunbie07@gamil.com>
2025-04-10 17:03:04 +0900

    pythonData

---
+++ python/pythonData/p38_typeCasting.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+str1 = '100'
+stt2 = '200'
+str3 = '12.345'
+
+int1 = int(str1)
+int2 = int(stt2)
+float1 = float(str3)
+
+sum = int1 + int2
+print('result1 : ', sum)
+
+float2 = float(int1) + 35.2
+print('result2 : ', float2)
+
