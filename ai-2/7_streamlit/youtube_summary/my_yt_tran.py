758b385f60740e6f4a182ac99f6394da61ae8567
eunbie07 <eunbie07@gmail.com>
2025-06-02 18:05:21 +0900

    yt_tran

--- ai-2/7_streamlit/youtube_summary/my_yt_tran.py
+++ ai-2/7_streamlit/youtube_summary/my_yt_tran.py
@@ -4,7 +4,7 @@ from youtube_transcript_api.formatters import TextFormatter
 
 def get_youtube_video_info(video_url):
     ydl_opts = {
-        'cookies':'./data/cookies.txt',
+        'cookies' :'./data/cookies.txt',
         'noplaylist': True,
         'quiet': True,
         'no_warnings': True
@@ -13,11 +13,11 @@ def get_youtube_video_info(video_url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         video_info = ydl.extract_info(video_url, download=False)
         return {
-            'id': video_info.get('id'),
-            'title': video_info.get('title'),
+            'id' : video_info.get('id'),
+            'title' : video_info.get('title'),
             'upload_date': video_info.get('upload_date'),
-            'channel': video_info.get('channel'),
-            'duration': video_info.get('duration_string'),
+            'channel' : video_info.get('channel'),
+            'duration' : video_info.get('duration_string'),
         }
 
 def get_video_id(video_url):
@@ -32,7 +32,7 @@ def get_video_id(video_url):
 
 def get_transcript_from_youtube(video_url, lang='en'):
     video_id = get_video_id(video_url)
-    transcript_list= YouTubeTranscriptApi.list_transcripts(video_id)
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
     transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[lang])
 
     text_formatter = TextFormatter()
